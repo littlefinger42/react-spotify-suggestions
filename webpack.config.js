8eaac92be307dce85fa8c002472ae42a678fe961
littlefinger42 <lauriemail@protonmail.com>
2019-06-05 19:15:29 +0200

    Implemented html-loaders to webpack and fixed webpack building by adding a correct entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  entry: { home: path.resolve(__dirname, "src") + "/index.jsx" },
   module: {
     rules: [
       {
@@ -7,7 +11,21 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
