09c393974a35e8305f997e75f24ddbe41798dd74
littlefinger42 <lauriemail@protonmail.com>
2019-06-12 20:09:01 +0200

    Fixed getSpotifyTopTracks fetch request

--- src/utils/spotifyUtils.js
+++ src/utils/spotifyUtils.js
@@ -25,14 +25,14 @@ const spotifyUtils = {
       options = { ...options, ...(options.signal = abortController.signal) };
     }
 
-    return fetch("https://api.spotify.com/v1/me/top/", options)
+    return fetch("https://api.spotify.com/v1/me/top/tracks", options)
       .then(res => res.json())
       .then(result => result, error => error);
   },
   redirectToSpotifyLoginPage() {
     window.location.href = `https://accounts.spotify.com/authorize?client_id=${
       config.spotify.clientId
-    }&response_type=token&redirect_uri=${config.spotify.redirectUrl}`;
+    }&response_type=token&redirect_uri=${config.spotify.redirectUrl}&scope=user-top-read`;
   }
 };
 
