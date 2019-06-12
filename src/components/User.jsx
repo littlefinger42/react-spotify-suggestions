8570b066cafc64c4c032b43f662f328cbf7727a5
littlefinger42 <lauriemail@protonmail.com>
2019-06-12 18:19:02 +0200

    Added label "Username" to user.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import style from "styled-components";
+
 import Avatar from "./Avatar.jsx";
+
+const Label = style.strong`
+  text-decoration: underline;
+`
 
 class User extends React.Component {
   render() {
     return (
       <div>
-        <span>{this.props.username}</span>
+        <span><Label>Username:</Label> {this.props.username}</span>
         <Avatar src={this.props.imgUrl} alt={this.props.username + " avatar"} />
       </div>
     );
