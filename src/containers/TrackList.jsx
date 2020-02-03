d4846105e8b766eddf54e8798b3e4503396f88c9
Laurie Tendulkar <lauriemail@protonmail.com>
2020-02-03 14:21:36 +0100

    Improved trackList overflow

--- src/containers/TrackList.jsx
+++ src/containers/TrackList.jsx
@@ -7,7 +7,7 @@ import Track from "../containers/Track.jsx";
 const StyledList = styled.ul`
   padding: 0 0 ${style.sizeSm} 0;
   max-height: 400px;
-  overflow-y: scroll;
+  overflow-y: auto;
 `;
 
 class TrackList extends React.Component {
