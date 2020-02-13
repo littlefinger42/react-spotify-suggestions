b194a2fd33af9d3865f4ad33ebfc7de5c55ec5f3
Laurie Tendulkar <lauriemail@protonmail.com>
2020-02-13 12:47:34 +0100

    Reduced size of  audio component

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,12 +1,8 @@
 import styled from "styled-components";
-import { style } from "../config"
 
 const Audio = styled.audio`
   height: 24px;
   width: 100%;
-  @media ${style.device.tablet} {
-    height: 32px;
-  }
 `;
 
 export default Audio;
