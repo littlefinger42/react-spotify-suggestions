6492e878121299ce1a89eee2b571d4a11ed9cad2
Laurie Tendulkar <lauriemail@protonmail.com>
2020-02-21 21:06:14 +0100

    Upgrade input container to function component

--- src/containers/InputContainer.jsx
+++ src/containers/InputContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { style } from "../config";
 import { connect } from "react-redux";
@@ -81,180 +81,160 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-class InputContainer extends React.Component {
-  notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-  constructor(props) {
-    super(props);
-    this.abortController = new AbortController();
-    this.state = {
-      isLoading: false,
-      alert: {
-        type: "",
-        message: ""
-      },
-      tabOpen: "topTracks"
-    };
-  }
+function InputContainer(props) {
+  const [isLoading, setLoading] = useState(false);
+  const [tabOpen, setTabOpen] = useState("topTracks");
+  const [alert, setAlert] = useState({ type: "", message: "" });
+  const notes = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B"
+  ];
 
   /**
    * Uses spotify utils to fetch recommendations based on selected tracks in the state, then sets them to the state
    */
-  getRecommendations = () => {
-    const { user, addRecommendedTracks } = this.props;
+  const getRecommendations = () => {
+    const { user, addRecommendedTracks, selectedTracks } = props;
 
-    if (this.props.selectedTracks.length === 0) {
-      this.setState({
-        alert: {
-          type: "warning",
-          message: "Select some tracks as seeds first!"
-        }
+    if (selectedTracks.length === 0) {
+      setAlert({
+        type: "warning",
+        message: "Select some tracks as seeds first!"
       });
       return false;
     }
 
-    this.setState({ isLoading: true });
+    setLoading(true);
     spotifyUtils
       .getSpotifyRecommendations(
         user.accessToken,
-        this.props.selectedTracks.map(track => track.id),
-        user.touchedRecommendationParams,
-        this.abortController
+        selectedTracks.map(track => track.id),
+        user.touchedRecommendationParams
       )
       .then(response => {
         if (response.error) {
-          this.setState({
-            isLoading: false,
-            alert: { type: "danger", message: response.error }
-          });
+          setAlert({ type: "danger", message: response.error });
+          setLoading(false);
           return false;
         }
         addRecommendedTracks(response);
-        this.setState({
-          isLoading: false
-        });
+        setLoading(false);
       });
   };
 
-  toggleTab = tab =>
-    this.setState(prevState => ({
-      tabOpen: prevState.tabOpen === tab ? "" : tab
-    }));
+  const touchedParamsList = props.user.touchedRecommendationParams.map(
+    param => {
+      return (
+        <li key={param.id} style={{ marginTop: 10 }}>
+          {param.label}:{" "}
+          {param.id === "key"
+            ? notes[param.value - 1]
+            : `${Math.round(param.value / param.step)}/${param.range[1] /
+                param.step}`}
+        </li>
+      );
+    }
+  );
 
-  componentWillUnmount() {
-    this.abortController.abort();
-  }
-
-  render() {
-    const { isLoading, alert, tabOpen } = this.state;
-
-    const touchedParamsList = this.props.user.touchedRecommendationParams.map(
-      param => {
-        return (
-          <li key={param.id} style={{ marginTop: 10 }}>
-            {param.label}:{" "}
-            {param.id === "key"
-              ? this.notes[param.value - 1]
-              : `${Math.round(param.value / param.step)}/${param.range[1] /
-                  param.step}`}
-          </li>
-        );
-      }
-    );
-
-    return (
-      <InputContainerContainer>
-        <InputContainerHeader>
-          <InputContainerTitle>Input</InputContainerTitle>
-        </InputContainerHeader>
-        {alert.type && alert.message && (
-          <InputItem>
-            <Alert type={alert.type}>{alert.message}</Alert>
-          </InputItem>
-        )}
+  return (
+    <InputContainerContainer>
+      <InputContainerHeader>
+        <InputContainerTitle>Input</InputContainerTitle>
+      </InputContainerHeader>
+      {alert.type && alert.message && (
         <InputItem>
-          <InputItemHeader onClick={() => this.toggleTab("selectedTracks")}>
-            Selected Tracks ({this.props.selectedTracks.length})
-            {tabOpen === "selectedTracks" ? <MdExpandLess /> : <MdExpandMore />}
-          </InputItemHeader>
-          {tabOpen === "selectedTracks" &&
-            (this.props.selectedTracks.length > 0 ? (
-              <TrackList min tracks={this.props.selectedTracks} />
-            ) : (
-              <Alert type="warning">
-                Select some tracks from search, top tracks or recent track lists
-                to use as seeds to suggestions.
-              </Alert>
-            ))}
+          <Alert type={alert.type}>{alert.message}</Alert>
         </InputItem>
-        <InputItem>
-          <InputItemHeader
-            onClick={() => this.toggleTab("suggestionParameters")}
-          >
-            Suggestion Parameters (
-            {this.props.user.touchedRecommendationParams.length})
-            {tabOpen === "suggestionParameters" ? (
-              <MdExpandLess />
-            ) : (
-              <MdExpandMore />
-            )}
-          </InputItemHeader>
-          {tabOpen === "suggestionParameters" && (
-            <div>
-              <InputItem>
-                <RecommendationsSelector />
-              </InputItem>
-
-              <InputItem>
-                <ul>
-                  {touchedParamsList && touchedParamsList.length === 0 && (
-                    <li>None Selected</li>
-                  )}
-                  {touchedParamsList}
-                  {touchedParamsList && touchedParamsList.length > 0 && (
-                    <li style={{ marginTop: 10 }}>
-                      <Button handleClick={this.props.clearSearchParams}>
-                        Clear
-                      </Button>
-                    </li>
-                  )}
-                </ul>
-              </InputItem>
-            </div>
+      )}
+      <InputItem>
+        <InputItemHeader onClick={() => setTabOpen("selectedTracks")}>
+          Selected Tracks ({props.selectedTracks.length})
+          {tabOpen === "selectedTracks" ? <MdExpandLess /> : <MdExpandMore />}
+        </InputItemHeader>
+        {tabOpen === "selectedTracks" &&
+          (props.selectedTracks.length > 0 ? (
+            <TrackList min tracks={props.selectedTracks} />
+          ) : (
+            <Alert type="warning">
+              Select some tracks from search, top tracks or recent track lists
+              to use as seeds to suggestions.
+            </Alert>
+          ))}
+      </InputItem>
+      <InputItem>
+        <InputItemHeader onClick={() => setTabOpen("suggestionParameters")}>
+          Suggestion Parameters ({props.user.touchedRecommendationParams.length}
+          )
+          {tabOpen === "suggestionParameters" ? (
+            <MdExpandLess />
+          ) : (
+            <MdExpandMore />
           )}
-        </InputItem>
-        <InputItem>
-          <InputItemHeader onClick={() => this.toggleTab("topTracks")}>
-            Top Tracks
-            {tabOpen === "topTracks" ? <MdExpandLess /> : <MdExpandMore />}
-          </InputItemHeader>
-          {tabOpen === "topTracks" && <TopTracks />}
-        </InputItem>
-        <InputItem>
-          <InputItemHeader onClick={() => this.toggleTab("recentTracks")}>
-            Recent Tracks
-            {tabOpen === "recentTracks" ? <MdExpandLess /> : <MdExpandMore />}
-          </InputItemHeader>
-          {tabOpen === "recentTracks" && <RecentTracks />}
-        </InputItem>
-        <InputItem>
-          <InputItemHeader onClick={() => this.toggleTab("searchTracks")}>
-            Search Tracks
-            {tabOpen === "searchTracks" ? <MdExpandLess /> : <MdExpandMore />}
-          </InputItemHeader>
-          {tabOpen === "searchTracks" && <SearchTracks />}
-        </InputItem>
-        <InputItem>
-          <Button
-            disabled={this.props.selectedTracks.length < 1 || isLoading}
-            handleClick={this.getRecommendations}
-          >
-            Suggest tracks
-          </Button>
-          {isLoading && <Spinner />}
-        </InputItem>
-      </InputContainerContainer>
-    );
-  }
+        </InputItemHeader>
+        {tabOpen === "suggestionParameters" && (
+          <div>
+            <InputItem>
+              <RecommendationsSelector />
+            </InputItem>
+
+            <InputItem>
+              <ul>
+                {touchedParamsList && touchedParamsList.length === 0 && (
+                  <li>None Selected</li>
+                )}
+                {touchedParamsList}
+                {touchedParamsList && touchedParamsList.length > 0 && (
+                  <li style={{ marginTop: 10 }}>
+                    <Button handleClick={props.clearSearchParams}>Clear</Button>
+                  </li>
+                )}
+              </ul>
+            </InputItem>
+          </div>
+        )}
+      </InputItem>
+      <InputItem>
+        <InputItemHeader onClick={() => setTabOpen("topTracks")}>
+          Top Tracks
+          {tabOpen === "topTracks" ? <MdExpandLess /> : <MdExpandMore />}
+        </InputItemHeader>
+        {tabOpen === "topTracks" && <TopTracks />}
+      </InputItem>
+      <InputItem>
+        <InputItemHeader onClick={() => setTabOpen("recentTracks")}>
+          Recent Tracks
+          {tabOpen === "recentTracks" ? <MdExpandLess /> : <MdExpandMore />}
+        </InputItemHeader>
+        {tabOpen === "recentTracks" && <RecentTracks />}
+      </InputItem>
+      <InputItem>
+        <InputItemHeader onClick={() => setTabOpen("searchTracks")}>
+          Search Tracks
+          {tabOpen === "searchTracks" ? <MdExpandLess /> : <MdExpandMore />}
+        </InputItemHeader>
+        {tabOpen === "searchTracks" && <SearchTracks />}
+      </InputItem>
+      <InputItem>
+        <Button
+          disabled={props.selectedTracks.length < 1 || isLoading}
+          handleClick={getRecommendations}
+        >
+          Suggest tracks
+        </Button>
+        {isLoading && <Spinner />}
+      </InputItem>
+    </InputContainerContainer>
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(InputContainer);
